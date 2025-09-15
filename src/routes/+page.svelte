a523b5c04fb4d2da74b1d01b4c268c5476c2c4ea
ambersr <amber.schalker@hva.nl>
2025-09-15 10:48:04 +0200

    Component button ingeladen in +page.svelte #11

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     let { data } = $props(); // rune die data doorgeeft tussen page.server.js en page.svelte ("magische property")
+    import DarkButton from '$lib/components/DarkButton.svelte';
 
     const person = data.person; 
 
@@ -9,4 +10,4 @@
 <p>{person.bio}</p>
 <a href="{person.custom.socials.instagram}">Instagram</a>
 <a href="{person.custom.socials.github}">github</a>
-<a href="{person.custom.socials.linkedin}">linkedin</a>
+<a href="{person.custom.socials.linkedin}">linkedin</a>            <DarkButton href="mailto:amber.schalker@hva.nl">Contact</DarkButton>
