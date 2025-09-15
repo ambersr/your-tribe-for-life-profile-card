e187910660b7e52b99501b3c6e34f6d5c99b3516
ambersr <amber.schalker@hva.nl>
2025-09-15 10:49:55 +0200

    Eerste HTML opzet #6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,35 @@
     let { data } = $props(); // rune die data doorgeeft tussen page.server.js en page.svelte ("magische property")
     import DarkButton from '$lib/components/DarkButton.svelte';
 
-    const person = data.person; 
+    let { data } = $props();
+    const person = data.person;
 
 </script>
 
-<h1>{person.name}</h1>
-<p>{person.bio}</p>
-<a href="{person.custom.socials.instagram}">Instagram</a>
-<a href="{person.custom.socials.github}">github</a>
-<a href="{person.custom.socials.linkedin}">linkedin</a>            <DarkButton href="mailto:amber.schalker@hva.nl">Contact</DarkButton>
+<main>
+    <div class="left">
+        <img src="/images/portret-amber.jpg" alt="" width="200" height="300">
+        <section>
+            <h3>Contact opnemen</h3>
+            <DarkButton href="mailto:amber.schalker@hva.nl">Contact</DarkButton>
+        </section>
+    </div>
+
+    <div class="right">
+        <section class="info">
+            <h1>{person.name}</h1>
+            <p>{person.bio}</p>
+        </section>
+
+        <section class="socials">
+            <h2>My socials</h2>
+            <div class="share">
+                <a href="{person.custom.socials.instagram}"><img src="/images/instagram.svg" alt="Logo Instagram"></a>
+                <a href="{person.custom.socials.github}"><img src="/images/github.svg" alt="Logo Github"></a>
+                <a href="{person.custom.socials.linkedin}"><img src="/images/linkedin.svg" alt="Logo LinkedIn"></a>
+            </div>
+            
+        </section>
+    </div>
+    
+</main>
