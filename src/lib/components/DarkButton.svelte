782be245a661c2fcdbe546624294c92006e922f3
ambersr <amber.schalker@hva.nl>
2025-09-15 10:47:34 +0200

    Component `DarkButton.svelte` #11

---
+++ src/lib/components/DarkButton.svelte
@@ -0,0 +1,23 @@
+<script>
+    let href = "";
+</script>
+
+<a href={href}>
+  <slot/>
+</a>
+
+<style>
+    a {
+        padding: .6em 1em;
+        border: 1px solid var(--secondary-text);
+        background-color: var(--secondary-background);
+        color: var(--third-text);
+        transition: .2s ease-in-out;
+    }
+
+    a:hover {
+        background-color: var(--primary-background);
+        color: var(--secondary-text);
+        transition: .2s ease-in-out;
+    }
+</style>
