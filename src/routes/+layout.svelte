22b9480fe27d79aa01435c527736d8253d4bdfbe
ambersr <amber.schalker@hva.nl>
2025-09-15 11:10:38 +0200

    Eerste HTML opzet header #13

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import DarkButton from '$lib/components/DarkButton.svelte';
 
 	let { children } = $props();
 </script>
@@ -7,5 +8,15 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<header>
+	<a href="/"><h2>AS</h2></a>
+	<nav>
+		<a href="https://github.com/ambersr?tab=repositories">Projecten</a>
+		<a href="https://i-love-web-degg.onrender.com/">Portfolio</a>
+	</nav>
+
+	<DarkButton href="mailto:amber.schalker@hva.nl">Contact</DarkButton>
+</header>
 
 {@render children?.()}
